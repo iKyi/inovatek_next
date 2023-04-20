c0afe3677f03df941eff174a3b2b12edf6578154
iKyi <ikymetaverse@gmail.com>
2023-04-20 23:41:31 +0300

    + changes

--- src/lib/getStrapiFullImageData.ts
+++ src/lib/getStrapiFullImageData.ts
@@ -1,12 +1,14 @@
 import { getStrapiMedia } from "@/lib/media";
 
 const getStrapiFullImageData = (data: any) => {
-  if (!data?.data?.attributes?.width && !data.attributes.height) {
+  const widthValue = data?.data?.attributes?.width ?? data?.attributes?.width;
+
+  if (!widthValue) {
     return null;
   }
   return {
-    width: data?.attributes?.width ?? data.data.attributes.width,
-    height: data?.attributes?.height ?? data.data.attributes.height,
+    width: data?.attributes?.width ?? data?.data?.attributes?.width,
+    height: data?.attributes?.height ?? data?.data?.attributes?.height,
     url: getStrapiMedia(data),
   };
 };
